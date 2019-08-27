1ec924f2c9af4046eecc9341eb8e3e9f692602a7
Nikhilesh Chaudhary <nikhileshchaudhary@Nikhileshs-MacBook-Pro.local>
2019-08-26 20:55:44 -0700

    added

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from '../components/TodoItem';
+
+class Todos extends React.Component {
+
+    render() {
+        return this.props.todos.map((todo) => (
+            <TodoItem todo={todo} />
+        ));
+    }
+
+}
+export default Todos;
